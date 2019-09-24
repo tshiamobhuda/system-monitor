8c365e853b56cac7fc59fc81bb5628a729f2d310
Tshiamo <tshiamobhuda@gmail.com>
2019-09-24 10:42:43 +0200

    Added Js to show stats
    - calculate arc sweep-angle
    - convert memory used and total memory from bytes to kilobytes
    - calculate the percentage of memory use

--- app/index.js
+++ app/index.js
@@ -1,1 +1,20 @@
-console.log('Hello world!');
+import document from "document";
+import { memory } from "system";
+
+let nativeMemoryPie = document.getElementById("nativeMemoryPie");
+let nativeMemoryPiePercentage = document.getElementById("nativeMemoryPiePercentage");
+let nativeMemoryPieText = document.getElementById("nativeMemoryPieText");
+
+//Calculate native memory usage
+let memUsedKB = memory.native.used/1000;
+let memTotalKB = memory.native.total/1000;
+let ratioUsage = memUsedKB/memTotalKB;
+let percentageUsage = ratioUsage * 100;
+let sweepAngle = ratioUsage * 360;
+
+nativeMemoryPie.sweepAngle = parseInt(sweepAngle.toFixed(2));
+nativeMemoryPiePercentage.text = `${percentageUsage.toFixed(2)}%`;
+nativeMemoryPieText.text = `${memUsedKB.toFixed(2)} / ${memTotalKB.toFixed(2)} KB`;
+
+//Show results
+console.log(memUsedKB, memTotalKB, memUsedKB.toFixed(2), memTotalKB.toFixed(2), percentageUsage.toFixed(2), sweepAngle.toFixed(2));
